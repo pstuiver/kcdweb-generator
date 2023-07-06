530f4c53698f36ba45aa265500fd1ab91546164b
pstuiver <pstuiverx@gmail.com>
2023-07-06 23:20:48 +0200

    set output directory and paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,15 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'css/index.[hash][extname]',
+        },
+      },
+    },
+  },
   build: {
     output:'static',
     redirects:true,
